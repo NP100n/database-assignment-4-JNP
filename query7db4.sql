193dd5b38c1f9d9df6055d86c52cde4738614327
Nicko Plhak <107762252+NP100n@users.noreply.github.com>
2026-02-26 12:32:45 -0800

    Add SQL query to count artists with 10+ MPEG tracks

---
+++ query7db4.sql
@@ -0,0 +1,11 @@
+SELECT COUNT(*)  as TotalArtists
+FROM (
+	SELECT artists.Name
+	From artists
+	JOIN albums on albums.ArtistId = artists.ArtistId
+	JOIN tracks on tracks.AlbumId = albums.AlbumId
+	JOIN media_types on media_types.MediaTypeId = tracks.MediaTypeId
+	WHERE media_types.Name LIKE '%MPEG%'
+	GROUP BY artists.Name
+	HAVING COUNT(tracks.TrackId) >= 10
+)
